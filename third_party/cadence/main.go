f550412e60334774495f16df741d029319b279a3
Raymond Zhang <zhangraymond05@gmail.com>
2024-11-12 15:58:43 -0500

    Add boilerplate for creating compositeValue from cadence.

--- third_party/cadence/main.go
+++ third_party/cadence/main.go
@@ -1,10 +1,38 @@
 package main
 
-import "C"
+import (
+	"C"
+
+	"github.com/onflow/cadence/common"
+)
 
 //export DoSomething
 func DoSomething(str string) interface{} {
 	return C.CString("hello")
+}
+
+//export CreateComposite
+func CreateComposite(
+	moveLoc int,
+	moveKind int,
+	moveQualifiedIdentifier string,
+	//fields []interpreter.CompositeField,
+	moveAddress int,
+) interface{} {
+	// derive these fields from params
+	var runtime *MoveRuntime
+	var location common.Location
+	var kind common.CompositeKind
+	var address common.Address
+
+	return NewCompositeValue(
+		runtime,
+		location,
+		moveQualifiedIdentifier,
+		kind,
+		//fields
+		address,
+	)
 }
 
 func main() {}
